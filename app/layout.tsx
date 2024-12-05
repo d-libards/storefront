e2a76a59be93febfd5266dc8cfd0d8d0144b3db4
d-libards <dominiquelibardo09@gmail.com>
2024-12-06 01:48:48 +0800

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+const inter = Inter({ subsets: ['latin'] });
+import Container from '@/components/global/container';
+import Navbar from '@/components/navbar/Navbar';
+
+export const metadata: Metadata = {
+  title: 'Next Storefront',
+  description: 'A nifty store built with Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>
+        <Navbar />
+        <Container className='py-20'>{children}</Container>
+      </body>
+    </html>
+  );
+}
