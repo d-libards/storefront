e48d16f50febb914ee817faefd8bfa35f3751933
d-libards <dominiquelibardo09@gmail.com>
2024-12-08 04:31:05 +0800

    loading home

--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -17,7 +17,7 @@ function LoadingProduct() {
       <CardContent className='p-4'>
         <Skeleton className='h-48 w-full' />
         <Skeleton className='h-4 w-3/4 mt-4' />
-        <Skeleton className='h-4 w-3/4 mt-4' />
+        <Skeleton className='h-4 w-1/4 mt-4' />
       </CardContent>
     </Card>
   );
