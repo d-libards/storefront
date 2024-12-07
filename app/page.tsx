0d15663c798e98c1e82b66a49409d17e418caf91
d-libards <dominiquelibardo09@gmail.com>
2024-12-07 20:25:51 +0800

    setup other components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button';
+import FeaturedProducts from '@/components/home/FeaturedProducts';
+import Hero from '@/components/home/Hero';
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-3xl'>Store Starter</h1>
-      <Button variant='outline' size='lg' className='capitalize m-8'>
-        click me
-      </Button>
+      <Hero />
+      <FeaturedProducts />
     </div>
   );
 };
